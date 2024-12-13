135c63bc107a34ed3529aedc7caccd76a39ed284
ahyildirim <ahmetshelby@hotmail.com>
2024-12-14 02:19:15 +0300

    Map Read

---
+++ src/LIBFT/ft_strchr.c
@@ -0,0 +1,11 @@
+#include "../../includes/cub3d.h"
+
+char	*ft_strchr(const char *s, int c)
+{
+	if (*s == (char)c)
+		return ((char *)s);
+	while (*s++)
+		if (*s == (char)c)
+			return ((char *)s);
+	return (0);
+}
---
+++ src/LIBFT/ft_strncmp.c
@@ -0,0 +1,16 @@
+#include "../../includes/cub3d.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t			i;
+	unsigned char	*xs1;
+	unsigned char	*xs2;
+
+	xs1 = (unsigned char *) s1;
+	xs2 = (unsigned char *) s2;
+	i = -1;
+	while ((xs1[++i] || xs2[i]) && i < n)
+		if (xs1[i] != xs2[i])
+			return (xs1[i] - xs2[i]);
+	return (0);
+}
